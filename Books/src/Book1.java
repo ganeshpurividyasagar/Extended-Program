2a9e5374ee9578e847fcb216ad38af67f58abf1a
ganeshpurividyasagar <104072727+ganeshpurividyasagar@users.noreply.github.com>
2022-08-28 12:05:37 +0530

    Add files via upload

---
+++ Books/src/Book1.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Book1 {
+
+	//@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("Webdriver.chrome.driver","C:\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		
+		//To open the url
+		driver.get("https://tide.com/en-us");
+		
+		
+		
+		// open how to wash the clothes
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/div[3]/div/div/div/div[3]/a")).click();
+		
+		//Contact Us
+		
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/div[1]/div/div/div/div[2]/a[2]")).click();
+	}
+}
